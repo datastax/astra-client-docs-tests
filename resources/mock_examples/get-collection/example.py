15153dbfed5084b35cda2ef4ccd7fc449224bc10
Stefano Lottini <236640031+sl-at-ibm@users.noreply.github.com>
2026-01-14 18:29:36 +0100

    use mypy for 'compiles' test in Python and catch more trouble (#1)

---
+++ resources/mock_examples/get-collection/example.py
@@ -0,0 +1,7 @@
+from astrapy import DataAPIClient
+
+# Get an existing collection
+client = DataAPIClient()
+database = client.get_database("**API_ENDPOINT**", token="**APPLICATION_TOKEN**")
+
+collection = database.get_collection("bla")
