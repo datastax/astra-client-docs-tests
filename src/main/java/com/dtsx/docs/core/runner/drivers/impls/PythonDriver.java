b3f124d82b6d076ef4cce19216e90ea4fb1533fb
skedwards88 <skedwards88@gmail.com>
2026-01-30 13:03:50 -0700

    alt method

--- src/main/java/com/dtsx/docs/core/runner/drivers/impls/PythonDriver.java
+++ src/main/java/com/dtsx/docs/core/runner/drivers/impls/PythonDriver.java
@@ -15,7 +15,6 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.regex.Pattern;
 
 public class PythonDriver extends ClientDriver {
     public PythonDriver(String artifact) {
@@ -53,20 +52,49 @@ public class PythonDriver extends ClientDriver {
 
     @Override
     public String preprocessScript(BaseScriptRunnerCtx ignoredCtx, String content) {
-        return "import os\n\n" + content;
+        // Add JSON encoder for Data API types at the beginning of the script
+        String jsonEncoderPrelude = """
+            import os
+            import json as _json_module
+            from astrapy.data_types import DataAPITimestamp as _DataAPITimestamp
+            
+            class _DataAPIEncoder(_json_module.JSONEncoder):
+                def default(self, o):
+                    if isinstance(o, _DataAPITimestamp):
+                        return o.to_string()
+                    return super().default(o)
+            
+            def _contains_data_api_types(obj):
+                '''Recursively check if object contains Data API types'''
+                if isinstance(obj, _DataAPITimestamp):
+                    return True
+                elif isinstance(obj, dict):
+                    return any(_contains_data_api_types(v) for v in obj.values())
+                elif isinstance(obj, (list, tuple)):
+                    return any(_contains_data_api_types(item) for item in obj)
+                return False
+            
+            # Override print to automatically JSON-encode Data API types
+            _original_print = print
+            def print(*args, **kwargs):
+                json_args = []
+                for arg in args:
+                    if _contains_data_api_types(arg):
+                        json_args.append(_json_module.dumps(arg, cls=_DataAPIEncoder))
+                    else:
+                        json_args.append(arg)
+                _original_print(*json_args, **kwargs)
+            
+            """;
+
+        return jsonEncoderPrelude + content;
     }
 
     @Override
     public List<?> preprocessToJson(BaseScriptRunnerCtx ctx, OutputJsonifySourceMeta meta, String content) {
-        // Replace Python boolean literals with JSON equivalents
+        // The preprocessScript method now handles JSON encoding of Data API types,
+        // so we only need to handle Python boolean literals here
         String processed = content.replace(" True,", " true,").replace(" False,", " false,");
-
-        // Replace DataAPITimestamp objects with their ISO string representation
-        // Pattern matches: DataAPITimestamp(timestamp_ms=1732752000000 [2024-11-28T00:00:00.000Z])
-        // and extracts the ISO timestamp string
-        Pattern timestampPattern = Pattern.compile("DataAPITimestamp\\(timestamp_ms=\\d+ \\[([^\\]]+)\\]\\)");
-        processed = timestampPattern.matcher(processed).replaceAll("\"$1\"");
-
         return JacksonUtils.parseJsonRoots(processed, Object.class);
     }
 
