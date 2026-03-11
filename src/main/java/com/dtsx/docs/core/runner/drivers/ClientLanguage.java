09da539a68d3d7aad60cc585ac8ff9d894ce0363
toptobes <kavinpg@gmail.com>
2026-03-11 01:30:48 -0500

    remove the custom java jar override

--- src/main/java/com/dtsx/docs/core/runner/drivers/ClientLanguage.java
+++ src/main/java/com/dtsx/docs/core/runner/drivers/ClientLanguage.java
@@ -12,8 +12,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
-import static com.dtsx.docs.HelperCli.CLI_DIR;
-
 /// Represents the various client languages available in enum form.
 ///
 /// Useful, as `enum`s come with parsing and holding static information for free.
@@ -63,13 +61,6 @@ public enum ClientLanguage {
     private final @Nullable String defaultArtifact;
     private final Function<String, ClientDriver> mkDriver;
     private final SnapshotsReducer snapshotsReducer;
-
-    public @Nullable String defaultArtifact() {
-        if (this == JAVA) {
-            return "files(\"" + CLI_DIR.toAbsolutePath() + "/astra-db-java-2.1.7-patched.jar\")"; // TODO remove this once Java is patched
-        }
-        return defaultArtifact;
-    }
 
     public static List<String> names() {
         return Arrays.stream(ClientLanguage.values()).map(Enum::name).toList();
